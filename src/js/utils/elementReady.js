0508bccc278f5e9daebd46b459b1764b6b6aeaec
= <guihalbonik@gmail.com>
2024-10-02 17:48:05 +0300

    edit element ready

--- src/js/utils/elementReady.js
+++ src/js/utils/elementReady.js
@@ -1,13 +1,21 @@
-export async function elementReady(selector) {
+export async function elementReady(selector, parent = false) {
+	let block = document.querySelector(selector);
+
+	if (block) {
+		resolve(block);
+		return;
+	}
+
 	return new Promise((resolve) => {
 		const observer = new MutationObserver((mutations, obs) => {
-			const block = document.querySelector(selector);
+			block = parent ? parent.querySelector(selector) : document.querySelector(selector);
+
 			if (block) {
 				resolve(block);
 				obs.disconnect();
 			}
 		});
 
-		observer.observe(document.documentElement, { childList: true, subtree: true });
+		observer.observe(parent ? parent : document.documentElement, { childList: true, subtree: true });
 	});
 }
